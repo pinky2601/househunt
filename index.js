6d564f26b4151bdaca6ca90ac87d80de9ec72dea
dedipya <dedipyareddybeeravalli@gmail.com>
2026-06-30 18:46:03 +0530

    MongoDB connected successfully

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // ================= MONGODB CONNECT =================
+mongoose.connect(
+    "mongodb://dedipyareddy27_db_user:yjzwKRb9PY4ASB39@ac-n32pgx9-shard-00-00.1d3nshf.mongodb.net:27017,ac-n32pgx9-shard-00-01.1d3nshf.mongodb.net:27017,ac-n32pgx9-shard-00-02.1d3nshf.mongodb.net:27017/househunt?ssl=true&replicaSet=atlas-t248am-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+)
+    .then(() => console.log("MongoDB Connected Successfully"))
+    .catch(err => console.log("MongoDB Error:", err));
 
-// mongoose.connect("mongodb+srv://....")
-//     .then(() => console.log("MongoDB Connected ✅"))
-//     .catch(err => console.log(err));
 
 // ================= HOME =================
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ================= LOGIN =================
+
 app.post("/login", (req, res) => {
     const { username, password } = req.body;
 
@@ -29,6 +33,7 @@ app.post("/login", (req, res) => {
 });
 
 // ================= SEARCH =================
+
 app.get("/search", (req, res) => {
     const location = req.query.location;
 
@@ -38,6 +43,7 @@ app.get("/search", (req, res) => {
 });
 
 // ================= ADD HOME =================
+
 app.post("/add-home", (req, res) => {
     const { title, location, rent } = req.body;
 
@@ -53,6 +59,7 @@ app.post("/add-home", (req, res) => {
 });
 
 // ================= SERVER START =================
+
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
 });
